f277984dd1f8f95b2cd7dca5ef78d7316194cb2c
taideli <1007811421@qq.com>
2017-06-07 14:55:08 +0800

    add study-java and study-scala projects

---
+++ study-java8/src/main/java/com/tdl/study/java8/pkg1/hello/HelloJava.java
@@ -0,0 +1,7 @@
+package com.tdl.study.java8.pkg1.hello;
+
+public class HelloJava {
+    public static void main(String args[]) {
+        System.out.println("Hello Java8");
+    }
+}
---
+++ study-scala/src/main/scala/com/tdl/study/scala/pkg1/hello/HelloScala.scala
@@ -0,0 +1,7 @@
+package com.tdl.study.scala.pkg1.hello
+
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    print("Hello scala")
+  }
+}
