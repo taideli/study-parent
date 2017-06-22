cfc5884ff3b9aaae9071ded16af7116c1ca2e0fe
taideli <1007811421@qq.com>
2017-06-22 10:49:15 +0800

    complete zk example on zk home page

--- study-zk/src/main/java/com/tdl/study/zk/pkg1/zk/DataMonitor.java
+++ study-zk/src/main/java/com/tdl/study/zk/pkg1/zk/DataMonitor.java
@@ -3,14 +3,21 @@
  */
 package com.tdl.study.zk.pkg1.zk;
 
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
-public class DataMonitor {
+import java.util.Arrays;
+
+/**
+ *  monitors the data in the ZooKeeper tree
+ */
+public class DataMonitor implements Watcher, AsyncCallback.StatCallback {
     private ZooKeeper zk;
     private String znode;
     private Watcher chainedWatcher;
     private DataMonitorListener listener;
+    public boolean dead;
+    private byte[] prevData;
 
     public DataMonitor(ZooKeeper zk, String znode, Watcher chainedWatcher, DataMonitorListener listener) {
         this.zk = zk;
@@ -20,9 +27,72 @@ public class DataMonitor {
         zk.exists(znode, true, this, null);
     }
 
-    public boolean isDead() {
-        // TODO 未完成
-        return false;
+    @Override
+    public void processResult(int rc, String path, Object ctx, Stat stat) {
+        boolean exists;
+        switch (rc) {
+            case KeeperException.Code.Ok:
+                exists = true;
+                break;
+            case KeeperException.Code.NoNode:
+                exists = false;
+                break;
+            case KeeperException.Code.SessionExpired:
+            case KeeperException.Code.NoAuth:
+                dead = true;
+                listener.closing(rc);
+                return;
+            default:
+                // retry errors
+                zk.exists(znode, true, this, null);
+                return;
+        }
+
+        byte[] b = null;
+        if (exists) {
+            try {
+                b = zk.getData(znode, false, null);
+            } catch (KeeperException e) {
+                // we don't need to worry about recovering now, the watch
+                // callbacks will kick off any exception handling
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        if ((null == b && b != prevData) || (null != b && !Arrays.equals(prevData, b))) {
+            listener.exists(b);
+            prevData = b;
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        String path = event.getPath();
+        if (event.getType() == Watcher.Event.EventType.None) {
+            // we are being told that the state of the connection has changed
+            switch (event.getState()) {
+                case SyncConnected:
+                    // In this particular example we don't need to do anything
+                    // here - watches are automatically re-registered with
+                    // server and any watches triggered while the client was
+                    // disconnected will be delivered (in order of course)
+                    break;
+                case Expired:
+                    // It's all over
+                    dead = true;
+                    listener.closing(KeeperException.Code.SessionExpired);
+                    break;
+            }
+        } else {
+            if (null != path && path.equals(znode)) {
+                // something has changed on the node, let's find out
+                zk.exists(znode, true, this, null);
+            }
+        }
+        if (null != chainedWatcher) {
+            chainedWatcher.process(event);
+        }
     }
 
     public interface DataMonitorListener {
@@ -30,7 +100,7 @@ public class DataMonitor {
          * The existence status of the node has changed.
          * @param data
          */
-        void exists(byte data[]);
+        void exists(byte[] data);
 
         /**
          * The Zookeeer session is no longer valid.
--- study-zk/src/main/java/com/tdl/study/zk/pkg1/zk/Executor.java
+++ study-zk/src/main/java/com/tdl/study/zk/pkg1/zk/Executor.java
@@ -3,16 +3,28 @@
  */
 package com.tdl.study.zk.pkg1.zk;
 
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+/**
+ * This ZooKeeper client watches a ZooKeeper node for changes
+ * and responds to by starting or stopping a program
+ * Executor负责与zk的连接
+ */
 public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener {
-    private String filename;
-    private String[] exec;
+    private String znode;
     private ZooKeeper zk;
     private DataMonitor dm;
+    private String filename;
+    private String[] exec;
+    Process child;
 
     public static void main(String args[]) {
         if (args.length < 4) {
@@ -26,7 +38,7 @@ public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListe
         String exec[] = new String[args.length - 3];
         System.arraycopy(args, 3, exec, 0, exec.length);
         try {
-            new Executor().run();
+            new Executor(hostPort, znode, filename, exec).run();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,9 +53,86 @@ public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListe
     }
 
     @Override
+    public void process(WatchedEvent event) {
+        dm.process(event);
+    }
+
+    @Override
     public void run() {
         try {
             synchronized (this) {while (!dm.dead) wait();}
         } catch (InterruptedException ignored) {}
+    }
+
+    @Override
+    public void exists(byte[] data) {
+        if (null == data) {
+            if (null != child) {
+                System.out.println("Killing process");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+
+                }
+            }
+            child = null;
+        } else {
+            if (null != child) {
+                System.out.println("Stopping child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(filename);
+                fos.write(data);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                System.out.println("Starting child");
+                child = Runtime.getRuntime().exec(exec);
+                new StreamWriter(child.getInputStream(), System.out);
+                new StreamWriter(child.getErrorStream(), System.out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void closing(int rc) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    static class StreamWriter extends Thread {
+        OutputStream os;
+        InputStream is;
+
+        StreamWriter(InputStream is, OutputStream os) {
+            this.os = os;
+            this.is = is;
+            start();
+        }
+
+        @Override
+        public void run() {
+            byte[] b = new byte[80];
+            int rc;
+            try {
+                while ((rc = is.read(b)) > 0) {
+                    os.write(b, 0, rc);
+                }
+            } catch (IOException ignored) {}
+        }
     }
 }
