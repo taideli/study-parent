9471fbbd31ff23bd818128699b730d54916351d3
taideli <1007811421@qq.com>
2017-07-04 14:39:04 +0800

    mongodb connection

---
+++ study-mongodb/src/main/java/com/tdl/study/mongodb/pkg1/MongoDBJDBC.java
@@ -0,0 +1,70 @@
+package com.tdl.study.mongodb.pkg1;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDBJDBC {
+    public static void main(String[] args) {
+        try {
+            ServerAddress serverAddress = new ServerAddress("172.30.10.101", 22001);
+            List<ServerAddress> addrs = new ArrayList<>();
+            addrs.add(serverAddress);
+
+            MongoCredential credential =
+                    MongoCredential.createScramSha1Credential("people", "people", "".toCharArray());
+            List<MongoCredential> credentials = new ArrayList<>();
+            credentials.add(credential);
+
+            MongoClient mongoClient = new MongoClient(addrs, credentials);
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("people");
+            System.out.println("connect to database successfully");
+
+            // create collection
+            mongoDatabase.createCollection("test");
+
+            // get collection
+            MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+
+            // insert document
+            Document doc = new Document();
+            doc.append("title", "MongoDB")
+               .append("description", "database")
+               .append("likes", 100)
+               .append("by", "Fly");
+
+            List<Document> documents = new ArrayList<>();
+            documents.add(doc);
+            collection.insertMany(documents);
+//            collection.insertOne(doc);
+
+            //find documents
+            FindIterable<Document> findIterable = collection.find();
+            MongoCursor<Document> cursor = findIterable.iterator();
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next());
+            }
+
+            // update
+            collection.updateMany(Filters.eq("likes", 100), new Document("likes", "200"));
+
+
+            // delete
+            collection.deleteOne(Filters.eq("likes", 200));
+            collection.deleteMany(Filters.eq("likes", 200));
+
+            mongoClient.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
