f52b3f9b3bec0960df42acd0af7be92c283f1efd
taideli <1007811421@qq.com>
2017-07-11 12:07:30 +0800

    fix some bugs of wrapper output

--- study-core/src/main/java/com/tdl/study/core/io/Wrapper.java
+++ study-core/src/main/java/com/tdl/study/core/io/Wrapper.java
@@ -3,12 +3,13 @@ package com.tdl.study.core.io;
 import com.tdl.study.core.io.input.Input;
 import com.tdl.study.core.io.output.Enqueue;
 import com.tdl.study.core.io.output.Output;
+import com.tdl.study.core.lambda.Runnable;
 
 import java.util.stream.Stream;
 
 public interface Wrapper {
 
-    static <T> WrapOutput<T> wrap(Output<?> base, Enqueue<T> d) {
+    static <T> WrapOutput<T> wrap(Output<?> base, final Enqueue<T> d) {
         return new WrapOutput<T>(base) {
             @Override
             public long enqueue(Stream<T> items) {
@@ -43,6 +44,16 @@ public interface Wrapper {
         @Override
         public String name() {return base.name();}
 
-        // FIXME: 2017/7/7 implement openable interface's methods here
+        public void opening(Runnable handler) {this.base.opening(handler);}
+
+        public void closing(Runnable handler) {this.base.closing(handler);}
+
+        public void open() {this.base.open();}
+
+        public void close() {this.base.close();}
+
+        public boolean opened() {return base.opened();}
+
+        public boolean closed() {return base.closed();}
     }
 }
