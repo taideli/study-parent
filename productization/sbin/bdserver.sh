b7765adda583c3a57a6ed0abdefc335b5800bbad
taideli <1007811421@qq.com>
2017-07-16 23:42:43 +0800

    bdserver.sh main finished; add a print status function in util.sh; change line separator from CTRL to LF

--- productization/sbin/bdserver.sh
+++ productization/sbin/bdserver.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-function bdserver_start() {
-  echo ""
+
+function bdserver_start_all() {
+  echo "start all"
 }
 
-
-function bdserver_stop() {
-  echo ""
+function bdserver_stop_all() {
+  echo "stop all"
 }
 
-function bdserver_status() {
-
+function bdserver_restart_all() {
+  echo "restart all"
 }
+
+function bdserver_status_all() {
+  echo "status_all"
+}
+
+function bdserver_usage() {
+  cat << EOF
+$0 is a shell cluster manager, your can use it to manage your cluster.
+Usage: $0 <CMD> [SERVICE]
+    CMD    : start stop restart status
+    SERVICE: zookeeper namenode datanode
+
+EOF
+}
+
+CMD=""; SRV=""
+if [ $# -eq 0 ]; then bdserver_usage; exit 0
+elif [ $# -eq 1 ]; then CMD=$1; SRV="all"
+elif [ $# -eq 2 ]; then CMD=$1; SRV=$2; fi
+case ${CMD} in 'start');; 'stop');; 'restart');; 'status');; *) echo "unknown cmd: $CMD"; exit 1;; esac
+case ${SRV} in 'zookeeper');; 'datanode');; 'namenode');; 'all');; *) echo "unknown service: $SRV"; exit 1;; esac
+_CMD=bdserver_${CMD}_${SRV}; ${_CMD}
+
+
 
--- productization/sbin/util.sh
+++ productization/sbin/util.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 function print_status() {
-
+  if [ $# -ne 2 ]; then return 1; fi
+  s=$1; ss=$2; _PSSL=24; _PSSSL=8
+  if [ "$ss"x = "Running"x ]; then
+    printf " service \033[1m%-${_PSSL}s\033[0m \033[1;32m[ %-${_PSSSL}s]\033[0m\n" "$s" "$ss"
+  elif [ "$ss"x = "Stopped"x ]; then
+    printf " service \033[1m%-${_PSSL}s\033[0m \033[1;31m[ %-${_PSSSL}s]\033[0m\n" "$s" "$ss"
+  elif [ "$ss"x = "Failed"x ]; then
+    printf " service \033[1m%-${_PSSL}s\033[0m \033[1;5;31m[ %-${_PSSSL}s]\033[0m\n" "$s" "$ss"
+  else
+    printf " service \033[1m%-${_PSSL}s\033[0m \033[1;33m[ %-${_PSSSL}s]\033[0m\n" "$s" "Unknown"
+  fi
 }
+
+#print_status "namenode" "Running"
+#print_status "datanode" "Stopped"
+#print_status "zkServer abcd" "Failed"
+#print_status "rdsfsdfsdf abcd" "FailedDFASF"
