7fea2246640bd2b311c3123c00677fac49803e67
taideli <1007811421@qq.com>
2017-07-23 21:37:39 +0800

    csv input fix

--- study-hdfs/src/main/java/com/tdl/study/hdfs/io/pump/input/HDFSCsvInput.java
+++ study-hdfs/src/main/java/com/tdl/study/hdfs/io/pump/input/HDFSCsvInput.java
@@ -2,63 +2,44 @@ package com.tdl.study.hdfs.io.pump.input;
 
 import com.tdl.study.core.io.input.InputImpl;
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.charset.Charset;
-import java.util.Map;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * from
  */
 public class HDFSCsvInput extends InputImpl<CSVRecord> {
 
-    private FileSystem hdfs;
-    private Reader reader;
-    private CSVParser parser = null;
+    private AtomicInteger ptr = new AtomicInteger(0);
+    private List<CSVRecord> records;
 
-    public HDFSCsvInput(String path) {
+    public HDFSCsvInput(String path) throws IOException {
         super();
         Configuration conf = new Configuration();
-        try {
-            hdfs = FileSystem.get(conf);
-            reader = new InputStreamReader(hdfs.open(new Path(path)));
-            parser = new CSVParser(reader, CSVFormat.DEFAULT);
-        } catch (IOException e) {
-            throw new RuntimeException("Parse CSV from hdfs, ", e);
-        }
+        FileSystem hdfs = FileSystem.get(conf);
+        Reader reader = new InputStreamReader(hdfs.open(new Path(path)));
+        records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader).getRecords();
+        reader.close();
+        hdfs.close();
         open();
-    }
-
-    /**
-     * csv header map
-     * @return a copy of csv header map
-     */
-    public Map<String, Integer> getHeaderMap() {
-        return parser.getHeaderMap();
     }
 
     @Override
     protected CSVRecord dequeue() {
-        return parser.iterator().hasNext() ? parser.iterator().next() : null;
+        ptr.getAndIncrement();
+        return records.remove(0);
     }
 
     @Override
-    public void close() {
-        try {
-            reader.close();
-            parser.close();
-            hdfs.close();
-        } catch (IOException e) {
-            logger().error("failed to close, for ", e);
-        }
-        super.close();
+    public boolean empty() {
+        return records.size() > 0;
     }
 }
