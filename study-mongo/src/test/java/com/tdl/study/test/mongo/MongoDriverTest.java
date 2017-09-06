2899d7f24e533a44f81b756293cdfb49155dfa3d
taidl <taidl@hzcominfo.com>
2017-09-06 16:52:55 +0800

    move MongoDriverTest to test

--- study-mongo/src/test/java/com/tdl/study/test/mongo/MongoDriverTest.java
+++ study-mongo/src/test/java/com/tdl/study/test/mongo/MongoDriverTest.java
@@ -1,9 +1,12 @@
-package com.tdl.study.mongo.pkg1;
+package com.tdl.study.test.mongo;
 
 import com.mongodb.*;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.Arrays;
 
@@ -12,15 +15,16 @@ public class MongoDriverTest {
     public static void main(String[] args) {
         MongoClientOptions options = MongoClientOptions.builder()
                 .applicationName(MongoDriverTest.class.getSimpleName())
-                .connectTimeout(10)
-                .heartbeatSocketTimeout(3)
                 .build();
+
+        System.out.println("options:\n" + options.toString());
 //    MongoClient client = new MongoClient("172.16.16.232", options);
 
 //    MongoClient client = new MongoClient(new MongoClientURI("mongodb://172.16.16.232:27017"));
         MongoCredential credential = MongoCredential.createCredential("user4db", "db", "!@#QAZ123qaz".toCharArray());
 //        MongoClient client = new MongoClient("172.16.16.232", 27017);
-        MongoClient client = new MongoClient(new ServerAddress("172.16.16.232", 27017), Arrays.asList(credential));
+        MongoClient client = new MongoClient(new ServerAddress("172.16.16.232", 27017),
+                Arrays.asList(credential), options);
         MongoDatabase database = client.getDatabase("db");
         MongoCollection<Document> collection = database.getCollection("col");
 
@@ -32,11 +36,21 @@ public class MongoDriverTest {
                 .append("tags", Arrays.asList("apache", "kafka", "message"))
                 .append("likes", String.valueOf(99.9));
 
-        collection.insertOne(doc);
+//        collection.insertOne(doc);
 
-        System.out.println(collection.count());
+//        collection.updateOne(Filters.eq("title", "kafka"), doc);
+        FindIterable<Document> it = collection.find();
+        it.forEach((Block<Document>) document -> System.out.println(document.toJson()));
+        /*iterable.forEach((Consumer<Document>) document -> {
+            document.
+        });*/
+        client.close();
 
-//        collection.find().first().forEach((d, o) -> System.out.println(d + " " + o));
-        System.out.println(collection.find().first().toJson());
+        System.out.println("======");
+        Bson filtersAnd = Filters.and(Filters.eq("f1", "v1"), Filters.lt("f2", "99"));
+        Bson filtersOr = Filters.or(Filters.eq("f1", "v1"), Filters.lt("f2", "99"));
+        System.out.println(filtersAnd);
+        System.out.println(filtersOr);
+        System.out.println(Filters.and());
     }
 }
