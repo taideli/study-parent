6394a6f9eb516ad3b26b01db965d7fafb2f709e8
taideli <1007811421@qq.com>
2017-09-17 23:24:06 +0800

    simple test

---
+++ study-spring/src/main/java/com/tdl/study/spring/t2/DiConfig.java
@@ -0,0 +1,9 @@
+package com.tdl.study.spring.t2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tdl.study.spring.t2")
+public class DiConfig {
+}
