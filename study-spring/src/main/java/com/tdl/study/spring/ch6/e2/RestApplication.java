b7d35ddf704bdb0c59026522382e1f76a74df34b
taideli <1007811421@qq.com>
2017-09-19 17:05:12 +0800

    ch6.e2

--- study-spring/src/main/java/com/tdl/study/spring/ch6/e2/RestApplication.java
+++ study-spring/src/main/java/com/tdl/study/spring/ch6/e2/RestApplication.java
@@ -1,12 +1,16 @@
 package com.tdl.study.spring.ch6.e2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication /*application.properties只有放在resources根目录才能被自动找到*/
+@PropertySource("classpath:com/tdl/study/spring/ch6/e2/application.properties")
+// ↑ 指定配置文件的位置
 public class RestApplication {
 
     private static final String BANNER = "     _             _                        _             \n" +
@@ -18,14 +22,21 @@ public class RestApplication {
             "                      __/ |     | |                  __/ |\n" +
             "                     |___/      |_|                 |___/ ";
 
+    @Value("${book.author}")
+    private String bookAuthor;
+
+    @Value("${book.name}")
+    private String bookName;
+
     @RequestMapping("/")
     String index() {
-        return "Hello Spring Boot";
+//        return "Hello Spring Boot";
+        return "book name: " + bookName + ", author: " + bookAuthor;
     }
 
     public static void main(String[] args) {
 
-        SpringApplication application = new SpringApplication(RestApplication.class, args);
+        SpringApplication application = new SpringApplication(RestApplication.class);
         application.setBanner((environment, sourceClass, out) -> out.println(BANNER));
         application.run();
     }
