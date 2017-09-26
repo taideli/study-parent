203ebcee586c1cbd0584c6a14e5309aad2fd88dd
taidl <taidl@hzcominfo.com>
2017-09-26 15:37:34 +0800

    how to use kafka

--- study-kafka/src/main/java/com/tdl/study/kafka/Example.java
+++ study-kafka/src/main/java/com/tdl/study/kafka/Example.java
@@ -1,4 +1,54 @@
 package com.tdl.study.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Arrays;
+import java.util.Properties;
+
 public class Example {
+
+    public static void main(String[] args) {
+        new Thread(Example::producer, "producer").start();
+        new Thread(Example::consumer, "consumer").start();
+    }
+
+    static void producer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "172.16.16.232:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("topic", Integer.toString(i), Integer.toString(i)));
+        producer.close();
+    }
+
+    static void consumer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "172.16.16.232:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+//        consumer.subscribe(Arrays.asList("foo", "bar"));
+        consumer.subscribe(Arrays.asList("topic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
 }
