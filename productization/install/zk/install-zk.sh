9523fbe6023d62f2a0ffde999b5e464046d7d99e
taidl <taidl@hzcominfo.com>
2017-09-26 10:29:56 +0800

    script of install zookeeper

--- productization/install/zk/install-zk.sh
+++ productization/install/zk/install-zk.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-set zk_org_short_name=`tar -tf ${zk} | head -n 1 | sed 's/\/$//'`
-tar zxf ${zk} -C ${BG_INSTALL_HOME}
-set zk_short_name=zookeeper
-mv ${BG_INSTALL_HOME}/${zk_org_short_name} ${BG_INSTALL_HOME}/${zk_short_name}
+ZK_DIR=`extract_name ${zookeeper}`
+tar zxf ${zookeeper} -C ${INSTALL_HOME}
+ZH_HOME=${INSTALL_HOME}/zookeeper
+mv ${INSTALL_HOME}/${ZK_DIR} ${ZH_HOME}
 
-echo "\
+echo "
+
 # zookeeper settings
-export ZK_HOME=${BG_INSTALL_HOME}/${hadoop_short_name}
+export ZK_HOME=${INSTALL_HOME}/${zk_dir_name}
 export PATH=\$PATH:\$ZK_HOME/bin
 # settings for spark on yarn
-" >> ${custom_profile}
+" >> ${CUSTOM_PROFILE}
 
-unset zk_org_short_name
-unset zk_short_name
-source /etc/profile  # fix me, this will not effect
+# config zookeeper
+ZK_CONF_DIR=${ZK_HOME}/conf
 
-. ./config-zk.sh
+mv ${ZK_CONF_DIR}/zoo-simple.cfg ${ZK_CONF_DIR}/zoo.cfg
+ZK_DATA_DIR=${DATA_HOME}/zookeeper/data
+mkdir -p ${ZK_DATA_DIR}
+echo "1" > ${ZK_DATA_DIR}/myid  # TODO fix me, each slave has a myid file with different value
+ZK_DATA_LOG_DIR=${LOG_HOME}/zookeeper/datalog
+# 在conf 目录生成zookeeper-env.sh 内容是
+# #!/bin/bash
+# export ZOO_LOG_DIR=....
+
+sed -i "s/^dataDir=*/dataDir=${ZK_DATA_DIR}" ${ZK_CONF_DIR}/zoo.cfg
+sed -i "/^dataDir=/a\\dataLogDir=${ZK_DATA_LOG_DIR}" ${ZK_CONF_DIR}/zoo.cfg
+host_id=1
+for host in `echo ${zookeeper_hosts} | sed 's/,/ /g'`; do
+    echo "server.${host_id}=${host}:2888:38888" >> ${ZK_CONF_DIR}/zoo.cfg
+    ((host_id++))
+done
+sed -i "s/^zookeeper.log.dir=*/zookeeper.log.dir=${LOG_HOME}/zookeeper" ${ZK_CONF_DIR}/log4j.properties
+sed -i "s/^zookeeper.tracelog.dir=*/zookeeper.tracelog.dir=${LOG_HOME}/zookeeper/trace" ${ZK_CONF_DIR}/log4j.properties
