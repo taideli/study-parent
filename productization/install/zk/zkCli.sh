6b01af57753fe3b560284217fc85824c56ed8538
taidl <taidl@hzcominfo.com>
2017-09-28 10:49:47 +0800

    zkServer.sh and zkCli.sh for pseudo cluster

--- productization/install/zk/zkCli.sh
+++ productization/install/zk/zkCli.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+SCRIPT_BIN_DIR=`dirname $0`
+ZK_SERVER_HOME=${SCRIPT_BIN_DIR}/..
+
+CONFIG_DIRS=${ZK_SERVER_HOME}/server_*/conf
+ZK_CNN=""
+for port in `find ${CONFIG_DIRS} -name zoo.cfg | xargs egrep '^clientPort=[0-9]+[[:space:]]*$' | awk -F= '{print $NF}'`; do
+  ZK_CNN="${ZK_CNN}127.0.0.1:${port},"
+done
+ZK_CNN=${ZK_CNN%,*}
+
+if [ $# -eq 0 -o "$1" = '-h' -o "$1" = "--help" ]; then
+  echo "Usage: `basename $0` <ZK_ID> [args...]"
+  echo "Server: ${ZK_CNN}"
+  exit 1
+fi
+
+ZK_DIR=${ZK_SERVER_HOME}/server_$1
+if [ ! -d ${ZK_DIR} ]; then
+  echo "${ZK_DIR} not exist."
+  exit 1
+fi
+
+shift
+${ZK_DIR}/bin/zkCli.sh "$@"
--- productization/install/zk/zkServer.sh
+++ productization/install/zk/zkServer.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+SCRIPT_BIN_DIR=`dirname $0`
+ZK_SERVER_HOME=${SCRIPT_BIN_DIR}/..
+
+#for server in `ls $ZK_SERVER_HOME | grep server_ | awk -F_ '{print $2}'`; do
+#  echo "server: $server"
+#done
+
+CMD=$1
+for SRV_DIR in  `ls ${ZK_SERVER_HOME} | grep server_`; do
+  ${ZK_SERVER_HOME}/${SRV_DIR}/bin/zkServer.sh ${CMD}
+#  echo "$CMD $SRV_DIR $?"
+done
