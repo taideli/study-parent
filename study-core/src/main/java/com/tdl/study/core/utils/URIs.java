b6cccf9873254ca12d9c7d4da65b0c3e0d548751
taidl <taidl@hzcominfo.com>
2017-10-20 16:46:17 +0800

    finish URIs

--- study-core/src/main/java/com/tdl/study/core/utils/URIs.java
+++ study-core/src/main/java/com/tdl/study/core/utils/URIs.java
@@ -2,66 +2,103 @@ package com.tdl.study.core.utils;
 
 import org.jooq.lambda.tuple.Tuple2;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class URIs {
-    private String uri;
     static String SLASH = "/", BACKSLASH = "\\", COMMA = ",", SEMICOLON = ";", COLON = ":",
-            AT = "@", QUESTION_MARK = "?", AMPERSAND = "&", EQUAL = "=";
+            AT = "@", QUESTION_MARK = "?", AMPERSAND = "&", EQUAL = "=", JINHAO = "#", XINHAO = "*";
+    private static String DEFAULT_ENCODE = "UTF-8";
+
+    private String uri;
+
     private String schema;
     private String username;
     private String password;
     private List<Tuple2<String, Integer>> hosts = new ArrayList<>();
     private List<String> paths = new ArrayList<>();
     private Map<String, String> parameters = new HashMap<>();
-    public URIs(String uri) {
+    private String fragment;
+
+    public URIs(String uri) throws UnsupportedEncodingException {
         this.uri = uri;
         parse();
     }
 
-    private void parse() {
+    private void parse() throws UnsupportedEncodingException {
         String remain = uri;
-        String separator = null;
-        // schema
-        separator = COLON + SLASH + SLASH;
-        int index = remain.indexOf(separator);
-        schema = remain.substring(0, index);
-        remain = remain.substring(index + separator.length());
-        // authority
-        separator = AT;
-        index = remain.indexOf(separator);
-        String authority = remain.substring(0, index);
-        remain = remain.substring(index + separator.length());
-        separator = COLON;
-        index = authority.indexOf(separator);
-        username = authority.substring(0, index);
-        password = authority.substring(index + separator.length());
-        // hots
-        separator = SLASH;
-        index = remain.indexOf(separator);
-        String hostsAndPorts = remain.substring(0, index);
-        parseHostAndPort(hostsAndPorts);
-        remain = remain.substring(index + separator.length());
-        // paths
-        separator = QUESTION_MARK;
-        index = remain.indexOf(separator);
-        String paths = remain.substring(0, index);
-        remain = remain.substring(index + separator.length());
-        this.paths = Arrays.asList(paths.split(SLASH));
-        // parameters
-        String parameters = remain;
-        separator = AMPERSAND;
-        Stream.of(parameters.split(separator)).forEach(pair -> {
-            int idx = pair.indexOf(EQUAL);
-            this.parameters.put(pair.substring(0, idx), pair.substring(idx + EQUAL.length()));
-        });
-        // zookeeper://username:password@172.16.16.232:2181,172.16.16.232:2182/namespace/table?param1=xxx&param2=yyy
-        // jdbc:mysql://
+        String locator;
+        int index;
+
+        // parse fragment
+        locator = JINHAO;
+        index = remain.lastIndexOf(locator);
+        if (index > 0) { // has fragment
+            String fragment = remain.length() == locator.length() + index ? null : remain.substring(index + locator.length());
+            this.fragment = (null == fragment) ? null : URLDecoder.decode(fragment, DEFAULT_ENCODE);
+            remain = remain.substring(0, index);
+        }
+
+        // parse parameters
+        locator = QUESTION_MARK;
+        index = remain.indexOf(locator);
+        if (index > 0) {
+            String query = remain.length() == locator.length() + index ? null : remain.substring(index + locator.length());
+            parseParameters(query);
+            remain = remain.substring(0, index);
+        }
+
+        // parse schema
+        locator = COLON + SLASH + SLASH;
+        index = remain.indexOf(locator);
+        String schema;
+        if (index <= 0 || (schema = remain.substring(0, index)).isEmpty())
+            throw new IllegalStateException("schema is not set");
+        this.schema = URLDecoder.decode(schema, DEFAULT_ENCODE);
+        remain = remain.substring(index + locator.length());
+
+        // parse authority
+        locator = AT;
+        index = remain.indexOf(locator);
+        if (index > 0) {
+            String authority = remain.substring(0, index);
+            parseAuthority(authority);
+        }
+        remain = remain.substring(index + locator.length());
+
+        // parse host and port
+        locator = SLASH;
+        String[] hostsAndPaths = remain.split(SLASH, 2);
+        parseHostAndPort(hostsAndPaths[0]);
+        String paths = hostsAndPaths.length < 2 ? null : hostsAndPaths[1];
+        parsePaths(paths);
+        //  elasticsearch://username:password@192.168.1.108:9200,192.168.1.109:9200,192.169.1.110:9200/path1/path2/path3?param1=value&&param2=value2&param3&param4&#fragment
     }
 
-    private void parseHostAndPort(String hostAndPort) {
+    private void parseParameters(String query) throws UnsupportedEncodingException {
+        if (null == query || "".equals(query)) return;
+
+        for (String s : query.split(AMPERSAND)) {
+            if (s == null || s.isEmpty()) continue;
+            String[] pair = s.split(EQUAL, 2);
+            String key = URLDecoder.decode(pair[0], DEFAULT_ENCODE);
+            String value = pair.length < 2 ? null : URLDecoder.decode(pair[1], DEFAULT_ENCODE);
+            parameters.put(key, value);
+        }
+    }
+
+    private void parseAuthority(String authority) throws UnsupportedEncodingException {
+        if (null == authority || authority.isEmpty()) return;
+        String[] up = authority.split(COLON, 2);
+        username = URLDecoder.decode(up[0], DEFAULT_ENCODE);
+        password = up.length < 2 ? null : URLDecoder.decode(up[1], DEFAULT_ENCODE);
+    }
+
+    private void parseHostAndPort(String hostAndPort) throws UnsupportedEncodingException {
         String[] hAndPs = hostAndPort.split(COMMA, -1);
         String[][] array = new String[hAndPs.length][2];
         for (int i = 0; i < hAndPs.length; i++) {
@@ -75,6 +112,7 @@ public class URIs {
                 if (i == array.length - 1) throw new RuntimeException("No port assigned.");
                 array[i][1] = array[i + 1][1];
             }
+            array[i][0] = URLDecoder.decode(array[i][0], DEFAULT_ENCODE);
             try {
                 port = Integer.parseInt(array[i][1]);
             } catch (NumberFormatException e) {
@@ -86,7 +124,15 @@ public class URIs {
         System.out.println();
     }
 
-    public static URIs parse(String uri) {
+    private void parsePaths(String path) throws UnsupportedEncodingException {
+        if (null == path || path.isEmpty()) return;
+        for (String s : path.split(SLASH)) {
+            if (null == s || s.isEmpty()) continue;
+            paths.add(URLDecoder.decode(s, DEFAULT_ENCODE));
+        }
+    }
+
+    public static URIs parse(String uri) throws UnsupportedEncodingException {
         return new URIs(uri);
     }
 
@@ -113,29 +159,81 @@ public class URIs {
         return new ArrayList<>(hosts);
     }
 
-    /**
-     * starts from zero
-     * @param level
-     * @return
-     */
-    public String pathAt(int level) {
+    public Tuple2<String, Integer> getHostAt(int index) {
+        return hosts.get(index);
+    }
+
+    public String getHostsAsString() {
+        return hosts.stream().map(tuple -> tuple.v1() + COLON + tuple.v2()).collect(Collectors.joining(COMMA));
+    }
+
+    public List<String> getPaths() {
+        return new ArrayList<>(paths);
+    }
+
+    public String getPathAt(int level) {
         return level >= paths.size() ? null :paths.get(level);
     }
 
+    public String getPathsAsString() {
+        return paths.stream().reduce("", (s1, s2) -> s1 + SLASH + s2);
+    }
 
+    public Map<String, String> getParameters() {
+        return new HashMap<>(parameters);
+    }
 
-    public static void main(String[] args) {
-        URIs uri = parse("zookeeper://username:password@172.16.16.232:2181,172.16.16.232:2182/namespace/table?param1=xxx&param2=yyy");
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+
+    public String getParametersAsString() {
+        return parameters.entrySet().stream()
+                .map(entry -> entry.getKey() + ((null != entry.getValue()) ? EQUAL + entry.getValue() : ""))
+                .collect(Collectors.joining(AMPERSAND));
+    }
+
+    public String getFragment() {
+        return fragment;
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        URIs uri = parse("zookeeper://user+name:pass%21word@172.16.16.232:2181,172.16.16.232,172.16.16.232:2182/namespace/table?contry=%E6%B1%89%E5%AD%97#fragment");
         System.out.println(uri);
+        System.out.println(uri.getSchema());
+        System.out.println(uri.getUsername());
+        System.out.println(uri.getPassword());
+        System.out.println(uri.getHostsAsString());
+        System.out.println(uri.getParametersAsString());
+        System.out.println(uri.getFragment());
+
+        System.out.println();
+        System.out.println(uri.getHostAt(1));
+        System.out.println(uri.getParameter("contry"));
 
     }
 
+    private String decode(String origin, boolean encoded) {
+        try {
+            return encoded ? URLDecoder.decode(origin, DEFAULT_ENCODE) : origin;
+        } catch (UnsupportedEncodingException e) {
+            return origin;
+        }
+    }
 
     @Override
     public String toString() {
-        List<String> hostAndPort = hosts.stream().map(t -> t.v1() + ":" + t.v2()).collect(Collectors.toList());
-        String s =Strings.join(",", hostAndPort);
-        return schema + COLON + SLASH + SLASH + username + COLON + password + AT
-        + s;
+        return toString(false);
+    }
+    
+    public String toString(boolean decoded) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(decode(schema, decoded)).append(COLON).append(SLASH).append(SLASH);
+        if (null != getAuthority()) sb.append(decode(getAuthority(), decoded)).append(AT);
+        if (null != getHostsAsString()) sb.append(decode(getHostsAsString(), decoded));
+        if (null != getPathsAsString()) sb.append(decode(getPathsAsString(), decoded));
+        if (null != getParametersAsString()) sb.append(QUESTION_MARK).append(getParametersAsString());
+        if (null != getFragment()) sb.append(JINHAO).append(decode(getFragment(), decoded));
+        return sb.toString();
     }
 }
