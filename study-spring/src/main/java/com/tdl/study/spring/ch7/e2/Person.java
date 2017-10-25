1cf90700e951ae4446e2561900ad5eab2b8faec6
taidl <taidl@hzcominfo.com>
2017-10-25 18:22:51 +0800

    spring ..

---
+++ study-spring/src/main/java/com/tdl/study/spring/ch7/e2/Person.java
@@ -0,0 +1,11 @@
+package com.tdl.study.spring.ch7.e2;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
