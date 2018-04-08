5bf3caf32cdd755325d173eb8dbcc25c79532d9e
taidl <taidl@hzcominfo.com>
2018-04-08 16:05:17 +0800

    inet address test

--- study-java/src/test/java/com/tdl/study/java/test/InetAddressTest.java
+++ study-java/src/test/java/com/tdl/study/java/test/InetAddressTest.java
@@ -1,14 +1,25 @@
 package com.tdl.study.java.test;
 
+import org.junit.Test;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class InetAddressTest {
-    public static void main(String[] args) throws UnknownHostException {
+
+    @Test
+    public void t0() throws UnknownHostException {
         //得到的是主机名
         System.out.println(InetAddress.getByName("172.16.16.232").getCanonicalHostName());
         // 得到的是主机别名
         System.out.println(InetAddress.getByName("172.16.16.232").getHostName());
         // 一般情况下得到的是一样的
+    }
+
+    @Test
+    public void t1() throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        System.out.println(address.getHostName());
+        System.out.println(address.getHostAddress());
     }
 }
